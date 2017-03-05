e49fb64e61d22b4e0dff8ad3ae0de2633ee6f400
lucifer <hoanglaravel@gmail.com>
2017-03-05 18:30:22 +0700

    topic seeder

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,11 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Topic::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->sentence($nbWords = 6, $variableNbWords = true),
+        'description' => $faker->text($maxNbChars = 300),
+        'teacher_id' => $faker->numberBetween($min = 2, $max = 201),
+    ];
+});
