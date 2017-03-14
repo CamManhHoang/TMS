af36330d0bc2342698dde804156bb920df303a92
lucifer <hoanglaravel@gmail.com>
2017-03-15 00:23:31 +0700

    dashboard links

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -9,7 +9,7 @@
     <div class="content bg-image overflow-hidden" style="background-image: url('assets/img/photos/photo3@2x.jpg');">
         <div class="push-50-t push-15">
             <h1 class="h2 text-white animated zoomIn">BẢNG ĐIỀU KHIỂN</h1>
-            <h2 class="h5 text-white-op animated zoomIn">Xin chào {{ Auth::user()->username }}</h2>
+            <h2 class="h5 text-white-op animated zoomIn">Xin chào {{ Auth::user()->is_teacher() ? Auth::user()->teacher->full_name : 'Administrator' }}</h2>
         </div>
     </div>
     <!-- END Page Header -->
@@ -17,41 +17,47 @@
     <!-- Page Content -->
     <div class="content">
         <div class="row">
-            <!-- Main Dashboard Chart -->
-            <div class="block">
-                <div class="block-header">
-                    <ul class="block-options">
-                        <li>
-                            <button type="button" data-toggle="block-option" data-action="refresh_toggle" data-action-mode="demo"><i class="si si-refresh"></i></button>
-                        </li>
-                    </ul>
-                    <h3 class="block-title">Weekly Overview</h3>
-                </div>
-
-                <div class="block-content text-center">
-                    <div class="row items-push text-center">
-                        <div class="col-xs-6 col-lg-3">
-                            <div class="push-10"><i class="si si-graph fa-2x"></i></div>
-                            <div class="h5 font-w300 text-muted">+ 205 Sales</div>
-                        </div>
-                        <div class="col-xs-6 col-lg-3">
-                            <div class="push-10"><i class="si si-users fa-2x"></i></div>
-                            <div class="h5 font-w300 text-muted">+ 25% Clients</div>
-                        </div>
-                        <div class="col-xs-6 col-lg-3 visible-lg">
-                            <div class="push-10"><i class="si si-star fa-2x"></i></div>
-                            <div class="h5 font-w300 text-muted">+ 10 Ratings</div>
-                        </div>
-                        <div class="col-xs-6 col-lg-3 visible-lg">
-                            <div class="push-10"><i class="si si-share fa-2x"></i></div>
-                            <div class="h5 font-w300 text-muted">+ 35 Followers</div>
-                        </div>
+            <div class="col-sm-6 col-lg-3">
+                <a class="block block-link-hover1 text-center" href="/research-topics">
+                    <div class="block-content block-content-full bg-info">
+                        <i class="fa fa-book fa-5x text-white"></i>
                     </div>
-                </div>
+                    <div class="block-content block-content-full block-content-mini">
+                        <strong>Chủ đề nghiên cứu</strong>
+                    </div>
+                </a>
             </div>
-            <!-- END Main Dashboard Chart -->
+            <div class="col-sm-6 col-lg-3">
+                <a class="block block-link-hover1 text-center" href="/student-topics">
+                    <div class="block-content block-content-full bg-primary">
+                        <i class="fa fa-list-ul fa-5x text-white"></i>
+                    </div>
+                    <div class="block-content block-content-full block-content-mini">
+                        <strong>Đề tài hướng dẫn</strong>
+                    </div>
+                </a>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <a class="block block-link-hover1 text-center" href="/pending-topics">
+                    <div class="block-content block-content-full bg-modern-dark">
+                        <i class="fa fa-list-alt fa-5x text-white"></i>
+                    </div>
+                    <div class="block-content block-content-full block-content-mini">
+                        <strong>Đề tài chờ xử lý</strong>
+                    </div>
+                </a>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <a class="block block-link-hover1 text-center" href="/teacher-info">
+                    <div class="block-content block-content-full bg-danger">
+                        <i class="si si-badge fa-5x text-white"></i>
+                    </div>
+                    <div class="block-content block-content-full block-content-mini">
+                        <strong>Thông tin cá nhân</strong>
+                    </div>
+                </a>
+            </div>
         </div>
-
     </div>
     <!-- END Page Content -->
 @endsection
