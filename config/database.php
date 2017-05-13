5032bb8c7693a62dafafcefb775ca568f33b0de4
manhcam <camhm@putatu.com>
2017-05-13 12:00:32 +0700

    env

--- config/database.php
+++ config/database.php
@@ -53,17 +53,15 @@ return [
         ],
 
         'mysql' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
+            'driver'    => 'mysql',
+            'host'      => env('DB_HOST', 'us-cdbr-iron-east-02.cleardb.net'),
+            'database'  => env('DB_DATABASE', 'heroku_f7469a0c6b39995'),
+            'username'  => env('DB_USERNAME', 'b419fdf6d21e11'),
+            'password'  => env('DB_PASSWORD', 'c78bdf37'),
+            'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
+            'prefix'    => '',
+            'strict'    => false,
         ],
 
         'pgsql' => [
